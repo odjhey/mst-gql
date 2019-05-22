a3010db47834a4a1a9c49dfbb3487455daa9e120
Michel Weststrate <mweststrate@gmail.com>
2019-05-22 12:26:28 +0200

    after demo

--- examples/3-twitter-clone/src/app/components/MessageWall.tsx
+++ examples/3-twitter-clone/src/app/components/MessageWall.tsx
@@ -1,5 +1,38 @@
 import React from "react"
+import { observer } from "mobx-react-lite"
 
 import { Loading, Error } from "./utils"
+import { useQuery } from "../models"
 
-export const MessageWall = () => <Loading />
+export const MessageWall = observer(() => {
+  const { loading, data, error } = useQuery(store =>
+    store.queryMessages(
+      {
+        offset: undefined,
+        count: 3,
+        replyTo: undefined
+      },
+      `__typename
+      id
+      text
+      user { 
+        __typename
+        id
+        name 
+      }`
+    )
+  )
+
+  if (loading) return <Loading />
+  if (error) return <Error>{error}</Error>
+  return (
+    <ul>
+      {data.map(message => (
+        <li>
+          {message.user.name}: {message.text}
+          <hr />
+        </li>
+      ))}
+    </ul>
+  )
+})
