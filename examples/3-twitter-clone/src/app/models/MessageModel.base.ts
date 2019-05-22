2b20ecf010ab4628e68184d53c9acbee35f2a00a
Michel Weststrate <mweststrate@gmail.com>
2019-05-22 14:52:42 +0200

    regenerated model

--- examples/3-twitter-clone/src/app/models/MessageModel.base.ts
+++ examples/3-twitter-clone/src/app/models/MessageModel.base.ts
@@ -3,9 +3,10 @@
 /* tslint:disable */
 
 import { types } from "mobx-state-tree"
-import { MSTGQLObject, MSTGQLRef } from "mst-gql"
+import { MSTGQLObject, MSTGQLRef, QueryBuilder } from "mst-gql"
 
 import { UserModel } from "./UserModel"
+import { UserModelSelector } from "./UserModel.base"
 import { MessageModel } from "./MessageModel"
 import { RootStore } from "./index"
 
@@ -21,6 +22,7 @@ export const MessageModelBase = MSTGQLObject
     timestamp: types.maybe(types.number),
     user: types.maybe(MSTGQLRef(types.late(() => UserModel))),
     text: types.maybe(types.string),
+    likes: types.optional(types.array(MSTGQLRef(types.late(() => UserModel))), []),
     replyTo: types.maybe(MSTGQLRef(types.late((): any => MessageModel))),
   })
   .views(self => ({
@@ -29,10 +31,21 @@ export const MessageModelBase = MSTGQLObject
     }
   }))
 
-export const messageModelPrimitives = `
-__typename
-id
-timestamp
-text
-`
+export class MessageModelSelector<PARENT=unknown> extends QueryBuilder<PARENT> {
+  get id() { return this.__attr(`id`) }
+  get timestamp() { return this.__attr(`timestamp`) }
+  get text() { return this.__attr(`text`) }
+  user(): UserModelSelector<this> { return this.__child(`user`, UserModelSelector) as any }
+  get userRef() { return this.__ref(`user`, UserModelSelector) }
+  likes(): UserModelSelector<this> { return this.__child(`likes`, UserModelSelector) as any }
+  get likesRef() { return this.__ref(`likes`, UserModelSelector) }
+  replyTo(): MessageModelSelector<this> { return this.__child(`replyTo`, MessageModelSelector) as any }
+  get replyToRef() { return this.__ref(`replyTo`, MessageModelSelector) }
+}
+
+export function selectFromMessage() {
+  return new MessageModelSelector()
+}
+
+export const messageModelPrimitives = selectFromMessage().id.timestamp.text.build()
 
