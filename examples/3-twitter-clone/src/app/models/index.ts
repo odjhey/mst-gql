5922a99b15a5146fddc7c1eb2f3a7ff91810a941
Michel Weststrate <mweststrate@gmail.com>
2019-05-22 11:58:35 +0200

    before-demo

--- examples/3-twitter-clone/src/app/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* This is a mst-sql generated file, don't modify it manually */
-/* eslint-disable */
-/* tslint:disable */
-
-export * from "./MessageModel"
-export * from "./UserModel"
-export * from "./RootStore"
-export * from "./reactUtils"
